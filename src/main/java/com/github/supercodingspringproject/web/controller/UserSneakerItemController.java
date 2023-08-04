730f368bb6f6fd9439c4c710e88daed3056ad139
VampireDeers <likemin5517@naver.com>
2023-08-05 06:28:05 +0900

    feat: 요구사항 2 구현

--- src/main/java/com/github/supercodingspringproject/web/controller/UserSneakerItemController.java
+++ src/main/java/com/github/supercodingspringproject/web/controller/UserSneakerItemController.java
@@ -2,11 +2,13 @@ package com.github.supercodingspringproject.web.controller;
 
 import com.github.supercodingspringproject.service.UserSneakerItemService;
 import com.github.supercodingspringproject.web.dto.SimpleSneakerInfo;
+import com.github.supercodingspringproject.web.dto.SneakerInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class UserSneakerItemController {
     @GetMapping("/sneakers")
     public Page<SimpleSneakerInfo> findPageableSneakers(Pageable pageable){
         return userSneakerItemService.findPopularSimpleSneakerInfo(pageable);
+    }
+
+    @GetMapping("/sneakers/{id}")
+    public SneakerInfo findPageableSneakers(@PathVariable("id") Integer modelId){
+        return userSneakerItemService.findSpecificModelInfo(modelId);
     }
 
 }
